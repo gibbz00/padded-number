253ffd49b4205f051ab05ba1498418089d841099
gibbz00 <gabrielhansson00@gmail.com>
2025-03-08 17:46:02 +0100

    chore: create facade crate

--- crates/internal/src/lib.rs
+++ crates/internal/src/lib.rs
@@ -1,21 +1,6 @@
 // TEMP: remove before first publish
 #![allow(missing_docs)]
 
-//! # `padded-number-padded-number` - Unsigned number with significant leading zeros
-//!
-//! Used when "0" and "00" should be considered as distinct values. Comes with
-//! ordering, addition, subtraction, and length bounds features included.
-//!
-//!
-//! Generic parameter `A` denotes the minimum digit count, inclusive.
-//! Generic parameter `B` denotes maximum digit count, inclusive.
-//!
-//! `A < B` allows for variable digit length.
-//! `A == B` requires the digit to exactly of length A (or B).
-//! `A > B, where A, B > 0` is technically declarable, but attempts at
-//! constructing a value will result in a runtime error.
-//! `A == 0` results in empty values being allowed as valid numbers. ("")
-
 mod core;
 pub use core::PaddedNumber;
 
